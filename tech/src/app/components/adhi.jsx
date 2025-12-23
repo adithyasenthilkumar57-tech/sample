f6b77821ddaef19cbbca6d75c433f115b49cc550
adithyasenthilkumar57-tech <adithyasenthilkumar57@gmail.com>
2025-12-23 19:26:46 +0530

    fix1

--- tech/src/app/components/adhi.jsx
+++ tech/src/app/components/adhi.jsx
@@ -4,7 +4,6 @@ export default function Adhi() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>Hello Adhi</div>
-            <img src="https://play-lh.googleusercontent.com/FPyNjPaFoIrF1OfhZpRLxjdYGkcqoqi8xdhKAf2yyuqgQ5IBOVNsgluVlfXDLpiFEBg" alt="" width={100} height={100} />
         </main>
     )
 }
