d68f4c5569194f0ee8dae7949d9b87b9315dcbeb
adithyasenthilkumar57-tech <adithyasenthilkumar57@gmail.com>
2025-12-23 19:11:13 +0530

    book,blog page created

---
+++ tech/src/app/book/page.jsx
@@ -0,0 +1,10 @@
+import Adhi from "../components/adhi";
+
+export default function Book() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>Hello Book</div>
+            <Adhi />
+        </main>
+    )
+}
---
+++ tech/src/app/components/adhi.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Adhi() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>Hello Adhi</div>
+            <img src="https://play-lh.googleusercontent.com/FPyNjPaFoIrF1OfhZpRLxjdYGkcqoqi8xdhKAf2yyuqgQ5IBOVNsgluVlfXDLpiFEBg" alt="" width={100} height={100} />
+        </main>
+    )
+}
